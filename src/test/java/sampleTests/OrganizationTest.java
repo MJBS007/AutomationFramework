bc349732d203387a976f7235d70b1a4dbe5b0113
Manu <Manu@DESKTOP-U6MU741>
2022-09-14 10:16:22 +0530

    First commit by Engg 2 - OrganizationTest

--- src/test/java/sampleTests/OrganizationTest.java
+++ src/test/java/sampleTests/OrganizationTest.java
@@ -1,5 +1,18 @@
 package sampleTests;
 
+import org.testng.annotations.Test;
+
 public class OrganizationTest {
+@Test
+public void org()
+{
+	System.out.println("The Organization");
+}
+@Test
+
+public void demotwo()
+{
+	System.out.println("This is Demo 2 ");
+}
 
 }
